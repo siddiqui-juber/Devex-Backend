220bf7d77334da8dc35382fcb743df2e9b7a63cd
siddiqui-juber <siddiquejubair147@gmail.com>
2026-05-15 16:54:36 +0530

    Initial commit

---
+++ src/main/java/Devex_Solutions/DevexBackendApplication.java
@@ -0,0 +1,13 @@
+package Devex_Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Devex_Solutions/DevexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Devex_Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
